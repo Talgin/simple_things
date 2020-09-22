23066464a4aae980ec9200709ec6e41d24219adf
Talgin <talgat90.07@gmail.com>
2020-09-22 14:14:11 -0400

    Add files via upload
    removing cudnn symbolic link error - need to revise version

---
+++ cudnn_symbolic_link.sh
@@ -0,0 +1,31 @@
+# short version
+# long version
+sudo rm libcudnn.so
+sudo rm libcudnn.so.8
+sudo ln libcudnn.so.8.0.3 libcudnn.so.8
+sudo ln libcudnn.so.8 libcudnn.so
+sudo rm libcudnn_cnn_train.so
+sudo rm libcudnn_cnn_train.so.8
+sudo ln libcudnn_cnn_train.so.8.0.3 libcudnn_cnn_train.so.8
+sudo ln libcudnn_cnn_train.so.8 libcudnn_cnn_train.so
+sudo rm libcudnn_adv_infer.so
+sudo rm libcudnn_adv_infer.so.8
+sudo ln libcudnn_adv_infer.so.8.0.3 libcudnn_adv_infer.so.8
+sudo ln libcudnn_adv_infer.so.8 libcudnn_adv_infer.so
+sudo rm libcudnn_ops_train.so
+sudo rm libcudnn_ops_train.so.8
+sudo ln libcudnn_ops_train.so.8.0.3 libcudnn_ops_train.so.8
+sudo ln libcudnn_ops_train.so.8 libcudnn_ops_train.so
+sudo rm libcudnn_cnn_infer.so
+sudo rm libcudnn_cnn_infer.so.8
+sudo ln libcudnn_cnn_infer.so.8.0.3 libcudnn_cnn_infer.so.8
+sudo ln libcudnn_cnn_infer.so.8 libcudnn_cnn_infer.so
+sudo rm libcudnn_ops_infer.so
+sudo rm libcudnn_ops_infer.so.8
+sudo ln libcudnn_ops_infer.so.8.0.3 libcudnn_ops_infer.so.8
+sudo ln libcudnn_ops_infer.so.8 libcudnn_ops_infer.so
+sudo rm libcudnn_adv_train.so
+sudo rm libcudnn_adv_train.so.8
+sudo ln libcudnn_adv_train.so.8.0.3 libcudnn_adv_train.so.8
+sudo ln libcudnn_adv_train.so.8 libcudnn_adv_train.so
+sudo ldconfig
